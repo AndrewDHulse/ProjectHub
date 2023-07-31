44b58fc5e08982b72dfaafc57be032f1582508f8
avilla13 <xetox13@gmail.com>
2023-07-31 16:33:02 -0500

    Add Project model and login form

--- projecthub/main_app/models.py
+++ projecthub/main_app/models.py
@@ -3,3 +3,15 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=50)
+    # Insert more attributes here:
+    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'project_id': self.id})
+    
--- projecthub/main_app/urls.py
+++ projecthub/main_app/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('projects/', views.projects_index, name='index'),
+    path('projects/create/', views.ProjectCreate.as_view(), name='projects_create'),
+    path('projects/<int:project_id>/', views.projects_detail, name='detail'),
+
 ]
