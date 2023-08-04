1829c57a182b3d4fb91dab501be90bb2632e4625
avilla13 <xetox13@gmail.com>
2023-08-04 00:33:00 -0500

    Add ProfilePhoto model, add_user_photo view & url, and photo upload to AWS S3 in user_profile.html (migration req)

--- main_app/models.py
+++ main_app/models.py
@@ -82,7 +82,7 @@ class Photo(models.Model):
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture= models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    profile_picture= models.ImageField( null=True, blank=True)
     bio = models.TextField(max_length=200)
     
     first_name = models.CharField(('first name'), max_length=150, blank=True)
@@ -91,3 +91,10 @@ class UserProfile(models.Model):
     
     def __str__(self):
         return self.user.username
+
+class ProfilePhoto(models.Model):
+    url = models.CharField(max_length=200)
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Photo for user_id: {self.user_id} @{self.url}"
